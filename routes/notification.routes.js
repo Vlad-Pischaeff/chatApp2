a2cc5f248c138eb0dfb8c3fcec83cf14119d2263
Vlad-Pischaeff <pischaeff@gmail.com>
2020-07-26 11:53:21 +0500

    fixes

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -50,17 +50,6 @@ router.get('/new', auth, async (req, res) => {
   }
 })
 
-// // get messages from public chat room
-// router.get('/chat/:id', auth, async (req, res) => {
-//   try {
-//     const id = req.params.id
-//     const messages = await Message.find({ to: id })
-//     res.status(201).json(messages)
-//   } catch(e) {
-//     res.status(500).json({ message:`Something wrong ..., details ${e}` })
-//   }
-// })
-
 // update notification information
 router.patch('/:id', auth, async (req, res) => {
   try {
@@ -73,82 +62,5 @@ router.patch('/:id', auth, async (req, res) => {
     res.status(500).json({ message:`Something wrong ..., details ${e}` })
   }
 })
-
-// // /api/auth/upload
-// router.post('/upload', async (req, res) => {
-//     try {
-//       res.status(201).json({})
-//     } catch (e) {
-//       res.status(500).json({ message:`Something wrong while uploading ${e}...` })
-//     }
-//   }
-// )
-
-// // /api/auth/search
-
-// router.post('/search', auth, async (req, res) => {
-//   try {
-//     const user = await User.findOne({ _id: req.user.userId })
-//     const friends = [...user.friends, req.user.userId]
-//     const users = await User.find({
-//                                     login: { $regex: req.body.search, $options: "i" },
-//                                       _id: { $nin: friends }
-//                                   })           
-//     res.status(201).json(users)
-//   } catch(e) {
-//     res.status(500).json({ message:`Something wrong ..., details ${e}` })
-//   }
-// })
-
-// // list of friends ready to invite /api/auth/friends
-
-// router.get('/friends', auth, async (req, res) => {
-//   try {
-//     const user = await User.findOne({ _id: req.user.userId })
-//     const friends = await User.find({ _id: user.friends })
-//     res.status(201).json(friends)
-//   } catch(e) {
-//     res.status(500).json({ message:`Something wrong ..., details ${e}` })
-//   }
-// })
-
-// // list of invited friends /api/auth/invited
-
-// router.post('/invited', auth, async (req, res) => {
-//   try {
-//     const invited = req.body.invited
-//     const friends = await User.find({ _id: invited })
-//     res.status(201).json(friends)
-//   } catch(e) {
-//     res.status(500).json({ message:`Something wrong ..., details ${e}` })
-//   }
-// })
-
-// // add new friends /api/auth/friends
-// router.patch('/friends', auth, async (req, res) => {
-//   try {
-//     const candidates = Object.values(req.body.friends)
-//     await User.updateMany({ _id: req.user.userId }, { $push: { friends: candidates } })
-//     const user = await User.findOne({ _id: req.user.userId })
-//     const friends = await User.find({ _id: user.friends })
-//     res.status(201).json(friends)
-//   } catch(e) {
-//     res.status(500).json({ message:`Something wrong ..., details ${e}` })
-//   }
-// })
-
-// // remove from friends /api/auth/unfollow/:id
-// router.patch('/unfollow/:id', auth, async (req, res) => {
-//   try {
-//     const id = req.params.id
-//     await User.updateOne({ _id: req.user.userId }, { $pull: { friends: id } })
-//     const user = await User.findOne({ _id: req.user.userId })
-//     const friends = await User.find({ _id: user.friends })
-//     res.status(201).json(friends)
-//   } catch(e) {
-//     res.status(500).json({ message:`Something wrong ..., details ${e}` })
-//   }
-// })
-
 
 module.exports = router
